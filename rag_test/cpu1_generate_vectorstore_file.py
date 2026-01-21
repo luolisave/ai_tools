c45c1f2a7160518ad8f6061c5c8efdf6a60ba10b
luolisave <luolisave@gmail.com>
2026-01-21 11:45:03 -0500

    update/fix rag CPU and CUDA example

---
+++ rag_test/cpu1_generate_vectorstore_file.py
@@ -0,0 +1,25 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFacePipeline
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from transformers import pipeline
+
+# 1. Documents
+docs = [
+    "Company annual leave is 10 days.",
+    "Leave requests must be submitted 3 days in advance.",
+    "Sick leave requires a doctor's note.",
+    "google password is gogl1234567890",
+    "facebook password is fdsfdsafsa",
+    "bank password is 7788414"
+]
+
+# 2. Embeddings + vector store
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+vectorstore = FAISS.from_texts(docs, embeddings)
+
+# Save to a directory
+vectorstore.save_local("vectorstore_db")
---
+++ rag_test/cpu2_use_vectorstore_file.py
@@ -0,0 +1,58 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFacePipeline
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+
+import transformers
+from transformers import pipeline
+
+print("transformers.__version__", transformers.__version__)
+
+
+# 2. Embeddings + vector store
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+# Load the saved vectorstore
+vectorstore = FAISS.load_local("vectorstore_db", embeddings, allow_dangerous_deserialization=True)# (search_kwargs={"k": 5})  will return 5 most relevant documents, default is 4
+# Now you can use it
+retriever = vectorstore.as_retriever()
+
+# 3. LLM (instruction-tuned)
+pipe = pipeline(
+    "text2text-generation",
+    model="google/flan-t5-base",
+    max_new_tokens=50
+)
+llm = HuggingFacePipeline(pipeline=pipe)
+
+# 4. Prompt
+prompt = PromptTemplate.from_template(
+    """Answer the question using only the context below.
+
+Context:
+{context}
+
+Question:
+{question}
+"""
+)
+
+# 5. RAG chain (LCEL)
+rag_chain = (
+    {
+        "context": retriever,
+        "question": RunnablePassthrough(),
+    }
+    | prompt
+    | llm
+)
+
+# 6. Run
+question = input("Your question: ").strip()
+if question:
+    result = rag_chain.invoke(question)
+    print(result)
+else:
+    print("No question entered.")
